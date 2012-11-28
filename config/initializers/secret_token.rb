2789c517dde400379ef38ceaadf455f0a82e88fb
Giles Adam Thomas <giles@gilesadamthomas.com>
2012-11-28 01:01:42 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Artbook::Application.config.secret_token = '986c8a582d7aef95268ac0cb2fb5fd70a929498440e03580ad9cf21581ac269330506dcbd4b6d5a7f5eb69fefe4f3360cccc543b70fbc17a23e19a745f057c6e'
